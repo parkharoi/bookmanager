605a776b179c58fb1660b0c536c8c8a1dd0d3b55
바카로이 <haeun8520@naver.com>
2025-06-05 18:36:29 +0900

    Spring Boot 프로젝트 전체 업로드

---
+++ src/test/java/org/example/bookmanager/BookmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
