ed3194ca32ff9f9986ae4b48e0e95ed86cccf9a0
바카로이 <haeun8520@naver.com>
2025-06-09 11:53:26 +0900

    ui 수정

--- src/main/java/org/example/bookmanager/config/SecurityConfig.java
+++ src/main/java/org/example/bookmanager/config/SecurityConfig.java
@@ -1,2 +1,48 @@
-package org.example.bookmanager.config;public class SecurityConfig {
+package org.example.bookmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/h2-console/**").permitAll() // H2 콘솔 허용
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**") // CSRF 예외 추가
+                )
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame
+                                .sameOrigin() //iframe 허용 (H2 콘솔은 iframe 사용 하는데 시큐리티가 막음)
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.builder()
+                .username("admin")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
 }
--- src/main/java/org/example/bookmanager/controller/HomeController.java
+++ src/main/java/org/example/bookmanager/controller/HomeController.java
@@ -1,2 +1,13 @@
-package org.example.bookmanager.controller;public class HomeController {
+package org.example.bookmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 }
